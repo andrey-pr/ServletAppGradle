e35d558d6beb6d917a6ddb0391ba90ee04a329c9
Andrey <andreykvak.g3@gmail.com>
2021-01-31 23:38:57 +0200

    Some fixes

--- src/main/java/app/servlets/FormServlet.java
+++ src/main/java/app/servlets/FormServlet.java
@@ -2,11 +2,13 @@ package app.servlets;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(value = "/sum")
 public class FormServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -16,9 +18,15 @@ public class FormServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String a = req.getParameter("a");
-        String b = req.getParameter("b");
-        String ab = a + b;
+        int a, b;
+        try {
+            a = Integer.parseInt(req.getParameter("a"));
+            b = Integer.parseInt(req.getParameter("b"));
+        } catch (NumberFormatException e) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+        int ab = a + b;
 
         req.setAttribute("a", a);
         req.setAttribute("b", b);
